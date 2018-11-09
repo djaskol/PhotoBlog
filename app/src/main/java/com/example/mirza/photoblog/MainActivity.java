419008cc98b6aea8846317224c1e5f6901d2c916
mkaramuratovic <djaskol@gmail.com>
2018-11-09 12:17:56 +0100

    Update in Project

--- app/src/main/java/com/example/mirza/photoblog/MainActivity.java
+++ app/src/main/java/com/example/mirza/photoblog/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -11,7 +14,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this , LoginActivity.class);
-        startActivity(intent);
+
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(currentUser == null){
+            Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
 }
