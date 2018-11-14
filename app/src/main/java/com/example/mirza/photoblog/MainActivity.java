2348c9de64c51d758b3b260444f69db05055f564
Mirza <djaskol@gmail.com>
2018-11-14 07:51:29 +0100

    Update on project

--- app/src/main/java/com/example/mirza/photoblog/MainActivity.java
+++ app/src/main/java/com/example/mirza/photoblog/MainActivity.java
@@ -3,31 +3,46 @@ package com.example.mirza.photoblog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mainToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainToolbar = (Toolbar) findViewById(R.id.main_tooolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("PhotoBlog");
 
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        if(currentUser == null){
-            Intent intent = new Intent(MainActivity.this , LoginActivity.class);
+        if (currentUser == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(intent);
             finish();
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+
+    }
 }
